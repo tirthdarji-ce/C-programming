5e11ebe51060b5c8f3ae5a376665f91e406bc828
tirthdarji-ce <tirthrdarji@gmail.com>
2025-07-21 14:21:55 +0530

    Add files via upload

---
+++ PRACTICAL2/Question.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int length=70;
+    int breath=90;
+    int area=length*breath;
+    int y=length+breath;
+    int perimeter=2*y;
+    printf("Area of the plot is\t:%d",area);
+    printf("\nperimeter of the plot is\t:%d",perimeter);
+    printf("\nThis Program is created by TIRTH_25TCEQWT\t");
+    return 0;
+}
---
+++ PRACTICAL2/second.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   int Num;
+   //Num=10;
+   printf("Enter the Number: ");
+   scanf("%d",&Num);
+   prinf
+   printf("This is Number %d" ,Num);
+   return 0;
+}
