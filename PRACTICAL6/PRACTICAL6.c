3d8a6ff80094d385b17eeea6a343639c19e6528e
tirthdarji-ce <tirthrdarji@gmail.com>
2025-07-28 15:47:23 +0530

    Add files via upload

---
+++ PRACTICAL6/PRACTICAL6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+
+{
+    float celsius;
+    printf("Temperature(celsius) :");
+    scanf("%f",&celsius);
+    float Fahrenheit=((celsius*9)/5)+32;
+    printf("\nThe Temperature in Fahrenheit is :%.2f",Fahrenheit);
+    printf("\n\nThis Program is created by TIRTH DARJI_25TCEQWT\t");
+    return 0;
+}
