76b892a974febbc014945c11cbffb2312ec2c062
tirthdarji-ce <tirthrdarji@gmail.com>
2025-08-04 14:32:14 +0530

    Add files via upload

---
+++ PRACTICAL8/PRACTICAL8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int age;
+    printf("enter your age:");
+    scanf("%d",&age);
+    if(age>=18){
+        printf("you are aligible to open account");
+    }
+    else{
+            printf("you are not aligible");
+    }
+    printf("\n this program is done by Tirth Darji,25TCQWT");
+return 0;
+}
